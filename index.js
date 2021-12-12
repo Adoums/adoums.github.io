ab7635108c9e05678f32c263d2ce5c52afcd3f89
adoums <66362633+Adoums@users.noreply.github.com>
2021-12-12 13:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const ws = new WebSocket("ws://message.adoums.repl.co/")
-const ifStored = () => {return window.sessionStorage.getItem("username")}
-if(ifStored()) ws.onopen = () => ws.send(`{"author":"${ifStored()}"}`)
-else ws.onopen = () => ws.send("[]")
-function createMessage(author, content) {
-    const div = document.createElement("div")
-    const authorElement = document.createElement("div")
-    const contentElement = document.createElement("div")
-    div.classList.add("message")
-    authorElement.classList.add("author")
-    contentElement.classList.add("content")
-    authorElement.innerText = author
-    contentElement.innerText = `> ${content}`
-    div.append(authorElement, contentElement)
-    document.getElementsByTagName("section")[0].append(div)
-}
-ws.onmessage = m => {
-    m = JSON.parse(m.data)
-    if(m.success) {
-        window.sessionStorage.setItem("username", m.author)
-    } else {
-        if(m instanceof Array) {
-            m = m.reverse()
-            for(let i = 0; i < m.length; i++) createMessage(m[i].author, m[i].content)
-        } else createMessage(m.author, m.content)
-        document.getElementsByTagName("section")[0].scrollTop = document.getElementsByTagName("section")[0].scrollHeight
-        if(document.hidden && m.loaded == false) new Audio('sound.mp3').play()
+;(function init() {
+    const ws = new WebSocket("wss://message.adoums.repl.co/")
+    const ifStored = () => {return window.sessionStorage.getItem("username")}
+    if(ifStored()) ws.onopen = () => ws.send(`{"author":"${ifStored()}"}`)
+    else ws.onopen = () => ws.send("[]")
+    function createMessage(author, content) {
+        const div = document.createElement("div")
+        const authorElement = document.createElement("div")
+        const contentElement = document.createElement("div")
+        div.classList.add("message")
+        authorElement.classList.add("author")
+        contentElement.classList.add("content")
+        authorElement.innerText = author
+        contentElement.innerText = `> ${content}`
+        div.append(authorElement, contentElement)
+        document.getElementsByTagName("section")[0].append(div)
     }
-}
-const textarea = document.getElementsByTagName("textarea")[0]
-textarea.onkeyup = e => {
-    if(!textarea.value.replace(/[^A-Za-z0-9]+/g, "").length) return
-    if(e.key == "Enter") {
-        const value = textarea.value.replace(/("|\n)/g, "")
-        textarea.value = ""
-        if(ifStored()) ws.send(`{"content":"${value}"}`)
-        else ws.send(`{"author":"${value}"}`)
+    ws.onmessage = m => {
+        m = JSON.parse(m.data)
+        if(m.success) {
+            window.sessionStorage.setItem("username", m.author)
+        } else {
+            if(m instanceof Array) {
+                m = m.reverse()
+                for(let i = 0; i < m.length; i++) createMessage(m[i].author, m[i].content)
+            } else createMessage(m.author, m.content)
+            document.getElementsByTagName("section")[0].scrollTop = document.getElementsByTagName("section")[0].scrollHeight
+            if(document.hidden && m.loaded == false) new Audio('sound.mp3').play()
+        }
     }
-}
+    const textarea = document.getElementsByTagName("textarea")[0]
+    textarea.onkeyup = e => {
+        if(!textarea.value.replace(/[^A-Za-z0-9]+/g, "").length) return
+        if(e.key == "Enter") {
+            const value = textarea.value.replace(/("|\n)/g, "")
+            textarea.value = ""
+            if(ifStored()) ws.send(`{"content":"${value}"}`)
+            else ws.send(`{"author":"${value}"}`)
+        }
+    }
+    ws.onclose = init
+})()
