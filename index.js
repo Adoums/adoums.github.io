684166f7be4453a52ced420872e7a0af284d8a57
adoums <66362633+Adoums@users.noreply.github.com>
2021-12-12 14:23:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 ;(function init() {
     const ws = new WebSocket("wss://message.adoums.repl.co/")
+    const section = document.getElementsByTagName("section")[0]
+    const textarea = document.getElementsByTagName("textarea")[0]
+    section.innerHTML = ""
     const ifStored = () => {return window.localStorage.getItem("username")}
     if(ifStored()) ws.onopen = () => ws.send(`{"author":"${ifStored()}"}`)
     else ws.onopen = () => ws.send("[]")
@@ -13,7 +16,7 @@
         authorElement.innerText = author
         contentElement.innerText = `> ${content}`
         div.append(authorElement, contentElement)
-        document.getElementsByTagName("section")[0].append(div)
+        section.append(div)
     }
     ws.onmessage = m => {
         m = JSON.parse(m.data)
@@ -24,11 +27,10 @@
                 m = m.reverse()
                 for(let i = 0; i < m.length; i++) createMessage(m[i].author, m[i].content)
             } else createMessage(m.author, m.content)
-            document.getElementsByTagName("section")[0].scrollTop = document.getElementsByTagName("section")[0].scrollHeight
+            section.scrollTop = section.scrollHeight
             if(document.hidden && m.loaded == false) new Audio('sound.mp3').play()
         }
     }
-    const textarea = document.getElementsByTagName("textarea")[0]
     textarea.onkeydown = e => {
         if(!textarea.value.replace(/[^A-Za-z0-9]+/g, "").length) return
         if(e.key == "Enter") {
